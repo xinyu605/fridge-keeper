dab2cc78248c2ab4fe704618d2a95abb965eafe8
Xinyu <xinyuyan605@gmail.com>
2024-08-02 13:52:10 +0800

    feat: add login button to app bar

--- src/app/StyledRoot.tsx
+++ src/app/StyledRoot.tsx
@@ -1,7 +1,13 @@
 'use client';
 import { type FC, type ReactNode, useMemo, useState } from 'react';
 
-import { AppBar, CssBaseline, Toolbar, type PaletteMode } from '@mui/material';
+import {
+  AppBar,
+  Button,
+  CssBaseline,
+  Toolbar,
+  type PaletteMode,
+} from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 
 import { darkTheme, lightTheme } from '@/styles/theme';
@@ -27,13 +33,15 @@ const StyledRoot: FC<StyledRootProps> = ({ children }) => {
       <CssBaseline />
       <body className={mode === 'dark' ? 'dark' : undefined}>
         <main className="flex flex-col min-h-screen bg-slate-100 dark:bg-slate-700">
-          <AppBar position="fixed" color="secondary">
+          <AppBar position="fixed" color="inherit">
             <Toolbar
               variant="dense"
               className="flex justify-end bg-white dark:bg-gray-700"
             >
-              <div>
+              <div className="flex gap-2 items-center">
                 <ThemeModeSwitch value={mode} onChange={handleChangeMode} />
+                {/* TODO: replace with i18n t function */}
+                <Button>LOGIN</Button>
               </div>
             </Toolbar>
           </AppBar>
