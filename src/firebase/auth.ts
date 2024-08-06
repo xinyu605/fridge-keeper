292e5827b2c173ffd60c213c6819cad05b44b341
Xinyu <xinyuyan605@gmail.com>
2024-08-06 12:09:36 +0800

    feat: implement signIn api

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -7,6 +7,11 @@ import firebaseApp from '@/firebase/config';
 
 const auth = getAuth(firebaseApp);
 
+/** More AuthErrorCodes see Firebase details: https://firebase.google.com/docs/reference/js/auth?hl=zh-tw#autherrorcodes  */
+export const AuthErrorCode = {
+  INVALID_LOGIN_CREDENTIALS: 'auth/invalid-credential',
+};
+
 export const signUp = async (email: string, password: string) => {
   const result = await createUserWithEmailAndPassword(auth, email, password);
   return result;
