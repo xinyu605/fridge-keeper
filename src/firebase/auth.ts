c9cbab2fde6446a6da59d50e5b28b400c5c563fb
Xinyu <xinyuyan605@gmail.com>
2024-08-06 10:42:59 +0800

    feat: firebase config and signUp, signIn api

---
+++ src/firebase/auth.ts
@@ -0,0 +1,18 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import firebaseApp from '@/firebase/config';
+
+const auth = getAuth(firebaseApp);
+
+export const signUp = async (email: string, password: string) => {
+  const result = await createUserWithEmailAndPassword(auth, email, password);
+  return result;
+};
+
+export const signIn = async (email: string, password: string) => {
+  const result = await signInWithEmailAndPassword(auth, email, password);
+  return result;
+};
