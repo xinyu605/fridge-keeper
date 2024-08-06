afb0b5925cf9b647ccbd102ebafe5a4e41402c49
Xinyu <xinyuyan605@gmail.com>
2024-08-06 09:41:03 +0800

    feat: adjust dark mode color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -44,17 +44,57 @@ export const darkTheme = createTheme({
     mode: 'dark',
     background: {
       ...lightTheme.palette.background,
-      paper: '#3f4739',
+      paper: '#0F172A' /** Tailwind class: slate-900 */,
     },
     primary: {
       main: '#fde047' /** Tailwind class: yellow-300 */,
-      dark: '#fef9c3' /**Tailwind class: yellow-100 */,
+      dark: 'rgba(251, 191, 36, 0.8)' /** Tailwind class: yellow-100 */,
       light: '#a16207' /** Tailwind class: yellow-700 */,
       contrastText: '#fff',
+    },
+    error: {
+      main: '#F87171' /** Tailwind class: red-400 */,
     },
     text: {
       ...lightTheme.palette.text,
       primary: '#fff',
+    },
+  },
+  components: {
+    ...lightTheme.components,
+    MuiButton: {
+      ...lightTheme.components?.MuiButton,
+      styleOverrides: {
+        ...lightTheme.components?.MuiButton?.styleOverrides,
+        root: {
+          '&.Mui-disabled': {
+            color: '#4B5563' /** Tailwind class: gray-600 */,
+          },
+        },
+        contained: {
+          color: '#334155' /** Tailwind class: slate-700 */,
+        },
+      },
+    },
+    MuiInputLabel: {
+      ...lightTheme.components?.MuiInputLabel,
+      styleOverrides: {
+        ...lightTheme.components?.MuiInputLabel?.styleOverrides,
+        root: {
+          color: '#64748B' /** Tailwind class: slate-500 */,
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      ...lightTheme.components?.MuiOutlinedInput,
+      styleOverrides: {
+        ...lightTheme.components?.MuiOutlinedInput?.styleOverrides,
+        input: {
+          ':-webkit-autofill': {
+            webBoxShadow: 'rgba(0, 0, 0, 0.5)',
+          },
+        },
+      },
     },
   },
 });
